2697618483ea62b04b6f3b471df6d4513a793ddd
Ed Wong <31254224+edmondwong6279@users.noreply.github.com>
2022-02-01 12:54:29 +0000

    Post feedback

--- app.js
+++ app.js
@@ -2,74 +2,68 @@
 // a zipped folder containing those files. At the moment this checks NOTHING
 // the uploaded files must have the name 'files' or it will not zip.
 
-const express   = require('express');
-const ruid = require('express-ruid');
-const multer    = require('multer');
-const archiver  = require('archiver');
-const rimraf = require('rimraf');
-const fs = require('fs')
+const express = require("express");
+const ruid = require("express-ruid");
+const multer = require("multer");
+const archiver = require("archiver");
+const fs = require("fs");
 
-const upload = multer({storage: multer.diskStorage({
-                            filename : (req, file, cb) => {
-                                cb(null, file.originalname);
-                            },
-                            destination: (req, file, cb) => {
-                                const dir = 'uploads/'+req.rid;
-                                if (!fs.existsSync(dir)){
-                                    fs.mkdir(dir, (err) => {if (err) throw err});
-                                }
-                                cb(null, './uploads/'+req.rid+'/');
-                            },
-                        })
+const upload = multer({
+    storage: multer.diskStorage({
+        filename: (req, file, cb) => {
+            cb(null, file.originalname);
+        },
+        destination: async (req, file, cb) => {
+            const dir = `uploads/${req.rid}`;
+            try {
+                await fs.promises.access(dir);
+            } catch (e) {
+                await fs.promises.mkdir(dir);
+            }
+            cb(null, `./uploads/${req.rid}/`);
+        },
+    }),
 });
 
 const port = 3000;
 const app = express();
 // for unique ID's with duplicate requests at the same time, defaults to / which normally
 // denotes subdirectories
-app.use(ruid({ setInContext: true, prefixSeparator: '-' }));
-
+app.use(ruid({ setInContext: true, prefixSeparator: "-" }));
 
 // deletes saved files and the directory they are in.
 const cleanUploads = async (uniqueId) => {
-    const files = await fs.promises.readdir('./uploads/'+uniqueId);
-    await Promise.all(files.map( file => {
-        fs.promises.unlink('./uploads/'+uniqueId+'/'+file)
-    }));
-    // A key point here is the maxBusyTries option. It appears that the default (3) is not enough if
-    // we spam requests in insomnia ie the directories are not deleted
-    await rimraf('./uploads/'+uniqueId+'/', {maxBusyTries: 10},  () => {
-        console.log('Cleaned upload directory: '+ uniqueId)});
-}
+    const files = await fs.promises.readdir(`./uploads/${uniqueId}`);
+    await Promise.all(
+        files.map(async (file) => {
+            await fs.promises.unlink(`./uploads/${uniqueId}/${file}`);
+        })
+    );
+    await fs.promises.rmdir(`./uploads/${uniqueId}/`);
+};
 
-
-const appendToArchive = async (archive, current) => {
-    return new Promise((res,rej) => {
-        const readStream = fs.createReadStream(current.path);
-        archive.append(readStream, { name: current.originalname})
-        res();
-    })
-}
-
-
-app.post('/', upload.array('files'), async (req,res)=> {
-    console.log('Received request. Id: '+req.rid);
+app.post("/", upload.array("files"), async (req, res) => {
+    console.log(`Received request. Id: ${req.rid}`);
     res.writeHead(200, {
-        'Content-Type': 'application/zip',
-        'Content-disposition': 'attachment; filename=myFile.zip'
+        "Content-Type": "application/zip",
+        "Content-disposition": "attachment; filename=myFile.zip",
     });
 
-    const archive = archiver('zip');
-    archive.pipe(res)
+    const archive = archiver("zip");
+    archive.on("end", async () => {
+        console.log("archive stream completed");
+        await cleanUploads(req.rid);
+    });
 
-    await Promise.all(req.files.map( file => {
-        appendToArchive(archive, file)
-    }))
+    archive.pipe(res);
+
+    req.files.forEach((file) => {
+        const readStream = fs.createReadStream(file.path);
+        archive.append(readStream, { name: file.originalname });
+    });
 
     archive.finalize();
-    await cleanUploads(req.rid);
 });
-
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
